373c1980bc5c8f943347dba7f9c30837dac0d783
Dua-Fatima-khan <officialduafatima64648@gmail.com>
2025-01-22 19:58:04 +0500

    share and search

--- src/components/FeaturedCard.tsx
+++ src/components/FeaturedCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import client from "@/sanity/lib/client";
+import  { client }  from "@/sanity/lib/client";
 import { useEffect, useState } from 'react';
 
 interface Product {
--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,8 +1,11 @@
 import { createClient } from 'next-sanity'
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || !process.env.NEXT_PUBLIC_SANITY_DATASET) {
+  throw new Error("Sanity project ID and dataset are required.");
+}
+
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  token: process.env.SANITY_API_TOKEN,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
-export default client
+  apiVersion: "2023-05-03",
+  useCdn: true,
+});
